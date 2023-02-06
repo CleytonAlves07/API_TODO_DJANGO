cba9de9fd91015578810ba377552a5286c7671de
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-06 18:26:35 -0300

    feat: :fire: Criando as routers em um novo arquivo urls.py

---
+++ todo/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo.views import UserViewSet, TaskViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet)
+router.register(r'tasks', TaskViewSet)
+
+
+urlpatterns = [
+    path(r'api/', include(router.urls)),
+]
