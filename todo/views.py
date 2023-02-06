5079d89eb3c31c655a22cbd874e2bbc7c6525f0d
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-06 18:24:26 -0300

    feat: :fire: Classes UserViewSet e TaskViewSet criadas para que a pessoa usuária possa interagir com a aplicação

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from todo.models import User, Task
+from todo.serializer import UserSerializer, TaskSerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
