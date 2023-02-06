1d5485c9c1e64113b1f5262acfeb1550af380037
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-06 18:17:14 -0300

    feat: :fire: Criando as models User e Task

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    senha = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_fineshed = models.BooleanField(default=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
